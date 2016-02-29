d5153ea372e447178dbd7d1034efd25c4c617065
Javier Islas García <javier@MacBook-Pro-de-Javier.local>
2016-02-29 11:06:55 -0600

    Se agrega la versión 1 del juego de memoria.

---
+++ Juego de Memoria.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+for numero in 0...100 {
+    if numero % 5 == 0 {
+        print("\(numero)Bingo!\n")
+    }
+    if numero % 2 == 0 {
+        print("\(numero)par!!!\n")
+    } else {
+        print("\(numero)impar!!!\n")
+    }
+    
+    if (numero >= 30 && numero <= 40 ) {
+        print("\(numero)Viva Swift!!!")
+    }
+}
